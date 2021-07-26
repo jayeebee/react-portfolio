910e9e6d222627acfbab5cb9c9c9df525d0e6baa
jayeebee <pnuthexsa311@gmail.com>
2021-07-26 02:05:49 -0400

    added final project

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, HashRouter as Router } from 'react-router-dom';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import About from '../../components/About'
 import Portfolio from '../../components/Portfolio';
 import Contact from '../../components/Contact'
@@ -11,9 +11,9 @@ export default function Nav() {
     return (
         <Router>
          <Switch>
-            <Route exact path="/" component={About} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
+            <Route path={process.env.PUBLIC_URL} exact path="/" component={About} />
+            <Route path={process.env.PUBLIC_URL} path="/about" component={About} />
+            <Route path={process.env.PUBLIC_URL} path="/contact" component={Contact} />
             <Route path="/portfolio" component={Portfolio} />
             <Route path="/resume" component={Resume} />
             <Route path="*" component={About} />
